cf4982a6650eb7478c9c0070fa4de54c10bdfcfd
AOx0 <50227494+AOx0@users.noreply.github.com>
2020-09-16 11:13:15 -0500

    Created Project's swift package

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_FactorialsTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_FactorialsTests.allTests()
+XCTMain(tests)
---
+++ Tests/swift-FactorialsTests/swift_FactorialsTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import swift_Factorials
+
+final class swift_FactorialsTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual("Nothing here yet", "Nothing here yet")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
